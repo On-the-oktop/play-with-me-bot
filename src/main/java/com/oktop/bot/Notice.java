7fedf14a0791a2468844096bf582f409e487bcc8
oktop <colaws3@gmail.com>
2019-10-04 15:18:30 +0900

    initial commit

---
+++ src/main/java/com/oktop/bot/Notice.java
@@ -0,0 +1,15 @@
+package com.oktop.bot;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class Notice {
+
+    private String text;
+
+}
